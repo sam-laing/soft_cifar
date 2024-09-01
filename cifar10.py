7f2fa41ed35dbc59ffac13874a2f19e03d67414c
Sam Laing <slaing155@gmail.com>
2024-09-01 12:53:32 +0200

    whole cifar loading

--- cifar10.py
+++ cifar10.py
@@ -4,19 +4,19 @@ import pickle
 from torch.utils.data import Dataset, DataLoader
 import torchvision.transforms as transforms
 from PIL import Image
+import torch
 
 def load_cifar10_batch(file):
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     images = dict[b'data']
     labels = dict[b'labels']
-    images = images.reshape(-1, 3, 32, 32).astype("float32")
+    images = images.reshape(-1, 32, 32,3).astype("float32")
     return images, labels
 
 def load_cifar10(root):
     train_images = []
     train_labels = []
-    
     for i in range(1, 6):
         file = os.path.join(root, f'data_batch_{i}')
         images, labels = load_cifar10_batch(file)
@@ -25,11 +25,17 @@ def load_cifar10(root):
     
     train_images = np.concatenate(train_images, axis=0)
     train_labels = np.array(train_labels)
+
+    max_indices = train_labels
+    labels = np.zeros((len(max_indices), 10))
+    np.put_along_axis(labels, max_indices[:, np.newaxis], 1, axis=1)
+    labels = torch.tensor(labels, dtype=torch.float)  
+
     
-    test_file = os.path.join(root, 'test_batch')
-    test_images, test_labels = load_cifar10_batch(test_file)
+    #test_file = os.path.join(root, 'test_batch')
+    #test_images, test_labels = load_cifar10_batch(test_file)
     
-    return (train_images, train_labels), (test_images, test_labels)
+    return (train_images, labels)    #, (test_images, test_labels)
 
 class CustomCIFAR10(Dataset):
     def __init__(self, images, labels, transform=None):
@@ -41,36 +47,99 @@ class CustomCIFAR10(Dataset):
         return len(self.images)
 
     def __getitem__(self, idx):
-        image = self.images[idx].transpose(1, 2, 0)  # Convert from CHW to HWC
-        image = Image.fromarray(np.uint8(image))  # Convert to PIL Image
+        # transpose to right order
+        image = self.images[idx]
+        label = self.labels[idx]
         
         if self.transform:
             image = self.transform(image)
         
         label = self.labels[idx]
         return image, label
+    
 
-
-if __name__ == '__main__':
+def make_larger_hard_datasets(path, split_ratio:list = [0.7, 0.15, 0.15], do_augmentation:bool = True, seed:int = 99):
     # Load the CIFAR-10 data from files
-    train_data, test_data = load_cifar10('/home/slaing/ML/2nd_year/sem2/research/data/cifar-10-batches-py')
+    data, labels = load_cifar10(path)
 
-    print(len(train_data[0]), len(test_data[0]))
+    N = len(data)
+    N_train, N_val = int(N*split_ratio[0]), int(N*split_ratio[1])
+    N_test = N - N_train - N_val
 
-    # Define transformations (optional)
-    transform = transforms.Compose([
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomCrop(32, padding=4),
-        transforms.ToTensor(),
+
+    np.random.seed(seed)
+    indices = np.random.permutation(N)
+
+    train_indices = indices[:N_train]
+    val_indices = indices[N_train:N_train+N_val]
+    test_indices = indices[N_train+N_val:]
+
+
+    if do_augmentation:
+        transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.ColorJitter(brightness=0.15, contrast=0.15, saturation=0.2, hue=0.15),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(10),
+            transforms.RandomResizedCrop(32, scale=(0.8, 1.0), ratio=(0.8, 1.2)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        ])
+    else:
+        transform = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
+        ])
+
+    non_train_transform = transforms.Compose([
+        transforms.ToTensor(), 
         transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2470, 0.2435, 0.2616)),
     ])
 
-    # Create custom dataset instances
-    train_dataset = CustomCIFAR10(images=train_data[0], labels=train_data[1], transform=transform)
-    test_dataset = CustomCIFAR10(images=test_data[0], labels=test_data[1], transform=transform)
+    train_dataset = CustomCIFAR10(images=data[train_indices], labels=labels[train_indices], transform=transform)
+    val_dataset = CustomCIFAR10(images=data[val_indices], labels=labels[val_indices], transform=non_train_transform)
+    test_dataset = CustomCIFAR10(images=data[test_indices], labels=labels[test_indices], transform=non_train_transform)
 
-    # Create DataLoader instances
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
-    test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+    return train_dataset, val_dataset, test_dataset
 
+def make_larger_hard_loaders(path, split_ratio:list = [0.7, 0.15, 0.15], do_augmentation:bool = True, batch_size:int = 64):
+    train_dataset, val_dataset, test_dataset = make_larger_hard_datasets(path, split_ratio, do_augmentation)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, val_loader, test_loader
+
+
+if __name__ == '__main__':
+    data, labels = load_cifar10('/mnt/qb/work/oh/owl886/datasets/cifar-10-batches-py')
+    print(data.shape, labels.shape)
+
+
+
+
+
+    path = "/mnt/qb/work/oh/owl886/datasets/cifar-10-batches-py"
+
+    train_dataset, val_dataset, test_dataset = make_larger_hard_datasets(path)
+
+    print(len(train_dataset), len(val_dataset), len(test_dataset))
+
+    train_loader, val_loader, test_loader = make_larger_hard_loaders(path)
+    for x,y in train_loader:
+        print(x.shape, y.shape)
+        break
+
+
+    '''
+    from model_structure import make_resnet_cifar
+    model = make_resnet_cifar(depth=20)
+    sample, slabel = train_dataset[0]
+    print(model(sample.unsqueeze(0)).shape)
+
+    for i, (input, label) in enumerate(train_loader):
+        print(model(input).shape)
+        break
+        
+    '''
 
